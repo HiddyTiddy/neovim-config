cbd6e6b138374dc2f44e4737cd5731bdc4a511a5
HiddyTiddy <hiddy.tiddey@gmail.com>
2023-02-19 22:43:09 +0100

    set random colorscheme

--- lua/hyde/colorscheme.lua
+++ lua/hyde/colorscheme.lua
@@ -1,3 +1,9 @@
+local cool_ones = { "nord", "catppuccin", "nordic" }
 
+---@diagnostic disable-next-line: param-type-mismatch
+math.randomseed(os.time(os.date("!*t")))
 
-vim.cmd [[ silent! colorscheme nord ]]
+local colorscheme_i = math.random(#cool_ones)
+
+vim.api.nvim_command("colorscheme " .. cool_ones[colorscheme_i])
+-- vim.cmd [[ silent! colorscheme nord ]]
